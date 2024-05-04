51b523b8face93c07352dd031124f7333825d4d1
Alexandar Dimcevski <solkpolk@gmail.com>
2024-05-04 11:39:36 -0400

    phoen sizing fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,13 @@ import 'tldraw/tldraw.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
      
       <h1 className="text-xl md:text-4xl font-bold">Alexander and Daniels <br/> Drawing Board</h1>
 
-      <div style={{ position: 'fixed', inset: 200 }}>
+      <div className="absolute inset-0 mb-0 mt-40">
           <Tldraw />
       </div>
-
 
     </main>
   );
