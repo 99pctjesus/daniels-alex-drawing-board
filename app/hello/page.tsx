f4744fa8189cdb62af739eb29c4012e4e558e69b
Alexandar Dimcevski <solkpolk@gmail.com>
2024-05-04 11:30:25 -0400

    drawing board

---
+++ app/hello/page.tsx
@@ -0,0 +1,15 @@
+
+
+/// 1. Create a Button 
+/// 2.
+
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       
+        <p>Hello Alex</p>
+  
+      </main>
+    );
+  }
+  
